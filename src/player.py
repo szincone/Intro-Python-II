8e8f5db39a5ce1611e7c5ac4fc2813d9091cd1ac
szincone <sawyerzincone@gmail.com>
2019-03-19 16:23:09 -0600

    cleaned str_ouput player, finished comments in adv.py

--- src/player.py
+++ src/player.py
@@ -4,8 +4,8 @@
 
 class Player():
     def __init__(self, name, current_room):
-        self.name = name
+        self.player_name = name
         self.current_room = current_room
 
     def __str__(self):
-        return (f"{self.name} is currently in {self.current_room}")
+        return (f"{self.player_name} is currently in {self.current_room}")
