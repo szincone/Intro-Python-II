3eb6deb9f0b77b0bf5763774d4a745b0caaa3668
szincone <sawyerzincone@gmail.com>
2019-03-20 13:43:50 -0600

    added player inventory

--- src/player.py
+++ src/player.py
@@ -1,11 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from item import Item
 
 
-class Player():
-    def __init__(self, name, current_room):
+class Player(Item):
+    def __init__(self, name, current_room, items=['empty...']):
+        super().__init__(items)
         self.player_name = name
         self.current_room = current_room
 
     def __str__(self):
         return (f"{self.player_name} is currently in {self.current_room.name}")
+
+    def inventory(self):
+        return (f"Loot: {', '.join([item for item in self.items])}.")
