c5f7c3a1b3332a3cba1b64f2f82211585e2d61bf
szincone <sawyerzincone@gmail.com>
2019-03-21 15:31:47 -0600

    mapped directions to word versions

--- src/room.py
+++ src/room.py
@@ -10,12 +10,14 @@ class Room(Item):
         self.description = description
 
     def list_items(self):
-        return (f"Loot: {', '.join([item for item in self.items])}.")
+        return (f"Loot: {', '.join([item.capitalize() for item in self.items])}.")
 
     def get_directions(self, curr_room):
-        directions = {"n_to": 'north', "w_to": 'north',
-                      "s_to": 'north', "e_to": 'north'}
+        directions = {"n_to": 'north', "w_to": 'west',
+                      "s_to": 'south', "e_to": 'east'}
         for key in curr_room.__dict__:
-            exits = [x for x in directions if x in curr_room.__dict__]
-
-        return ", ".join([exit for exit in exits])
+            # exits = [x for x in directions if x in curr_room.__dict__]
+            exits = [directions[x]
+                     for x in directions if x in curr_room.__dict__]
+        # print([directions[x] for x in directions ])
+        return ", ".join([exit.capitalize() for exit in exits])
